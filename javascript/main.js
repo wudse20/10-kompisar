ee90ac385e03e0e4faea0f5fbf33b9f9ef70f604
Anton Skorup <antonskorup@gmail.com>
2020-12-23 10:54:23 +0100

    Added logic for init of game

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,26 @@
+"use strict";
+let questions = [];
+
 function swapPage(path) {
     location.replace(path);
+}
+
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+function init(length) {
+    let r1 = document.getElementById("row-1");
+    let r2 = document.getElementById("row-2");
+
+    r1.style.display = "none";
+    r2.style.display = "block";
+
+    questions = [];
+    for (let i = 0; i < length; i++) {
+        questions.push(generateRandomNumber());
+        console.log(questions[i]);
+    }
+
+    console.log(questions)
 }
