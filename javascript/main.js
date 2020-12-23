041fe248cd840cff97c3775ff47f12d8b38b2066
Anton Skorup <antonskorup@gmail.com>
2020-12-23 11:12:47 +0100

    Made game for 10 questions

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,8 @@
 "use strict";
 let questions = [];
+let count = 0;
+let correct = 0;
+
 
 function swapPage(path) {
     location.replace(path);
@@ -12,15 +15,43 @@ function generateRandomNumber() {
 function init(length) {
     let r1 = document.getElementById("row-1");
     let r2 = document.getElementById("row-2");
+    let r3 = document.getElementById("row-3");
 
     r1.style.display = "none";
     r2.style.display = "block";
-
+    r3.style.display = "block";
+    
     questions = [];
+    count = 0;
+    correct = 0;
     for (let i = 0; i < length; i++) {
         questions.push(generateRandomNumber());
         console.log(questions[i]);
     }
 
-    console.log(questions)
+    document.getElementById("question").innerHTML = "Fråga " + (count + 1) + ": " + questions[count];
+}
+
+function submitAnswer() {
+    let input = document.getElementById("inp");
+    let value = parseInt(input.value);
+
+    if (!isNaN(value) && value == (10 - questions[count]))
+        correct++;
+    
+    if (++count < 10) {
+        document.getElementById("question").innerHTML = "Fråga " + (count + 1) + ": " + questions[count];
+    } else {
+        let r1 = document.getElementById("row-1");
+        let r2 = document.getElementById("row-2");
+        let r3 = document.getElementById("row-3");
+
+        r1.style.display = "block";
+        r2.style.display = "none";
+        r3.style.display = "none";
+
+        alert("Du fick: " + correct + " rätt av 10!");
+    }
+
+    input.value = "";
 }
