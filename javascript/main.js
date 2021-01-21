216fbcfffe318318a5a95fb31faf2c71c08d6d6c
wudse20 <antonskorup@gmail.com>
2021-01-21 19:08:13 +0100

    Added addition

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,43 @@
 "use strict";
+
+class Question 
+{
+    constructor(i1, ans) 
+    {
+        this.i1 = i1;
+        this.ans = ans;
+    }
+
+    isCorrect(inp)
+    {
+        return !isNaN(parseInt(inp)) && (inp + this.i1 == this.ans);
+    }
+
+    toString()
+    {
+        return this.i1 + " + _ = " + this.ans;
+    }
+}
+
 let questions = [];
 let count = 0;
 let correct = 0;
-let length;
+let length = 0;
 
-
-function swapPage(path) {
+function swapPage(path) 
+{
     location.replace(path);
 }
 
-function generateRandomNumber() {
-    return Math.floor(Math.random() * 10) + 1;
+function generateRandomNumber(max) 
+{
+    return Math.floor(Math.random() * max);
 }
 
-function init(_length) {
+function initTenBuddies(_length) 
+{
+    length = _length;
+
     let r1 = document.getElementById("row-1");
     let r2 = document.getElementById("row-2");
     let r3 = document.getElementById("row-3");
@@ -25,23 +49,80 @@ function init(_length) {
     questions = [];
     count = 0;
     correct = 0;
-    for (let i = 0; i < _length; i++) {
-        questions.push(generateRandomNumber());
+    for (let i = 0; i < _length; i++)
+    {
+        let number = generateRandomNumber(10) + 1;
+        questions.push(parseInt(number));
         console.log(questions[i]);
     }
 
-    length = _length;
 
     document.getElementById("question").innerHTML = "Fråga " + (count + 1) + ": " + questions[count];
 }
 
-function submitAnswer() {
+function initAddition(_length, max)
+{
+    length = _length;
+
+    let r1 = document.getElementById("row-1");
+    let r2 = document.getElementById("row-2");
+    let r3 = document.getElementById("row-3");
+
+    r1.style.display = "none";
+    r2.style.display = "block";
+    r3.style.display = "block";
+
+    questions = [];
+    correct = 0;
+    count = 0;
+
+    for (let i = 0; i < _length; i++) 
+    {
+        let i1 = generateRandomNumber(max);
+        let i2 = generateRandomNumber(max);
+        let ans = i1 + i2;
+        console.log(ans);
+        questions.push(new Question(+i1, ans));
+        console.log(questions[i].toString());    
+    }
+
+    document.getElementById("question").innerHTML = "Fråga " + (count + 1) + ": " + questions[count].toString();
+}
+
+function submitAnswer(isTenBuddies)
+{
     let input = document.getElementById("inp");
     let value = parseInt(input.value);
 
-    tenBuddies(value);
+    if (isTenBuddies)
+        tenBuddies(value);
+    else
+        addition(value);
     
     input.value = "";
+}
+
+function addition(value)
+{
+    if (!isNaN(value) && questions[count].isCorrect(value))
+        correct++;
+    
+    if (++count < length) 
+    {
+        document.getElementById("question").innerHTML = "Fråga " + (count + 1) + ": " + questions[count].toString();
+    }
+    else
+    {
+        let r1 = document.getElementById("row-1");
+        let r2 = document.getElementById("row-2");
+        let r3 = document.getElementById("row-3");
+
+        r1.style.display = "block";
+        r2.style.display = "none";
+        r3.style.display = "none";
+
+        alert("Du fick: " + correct + " rätt av 10!");
+    }
 }
 
 function tenBuddies(value)
@@ -49,9 +130,12 @@ function tenBuddies(value)
     if (!isNaN(value) && value == (10 - questions[count]))
         correct++;
     
-    if (++count < length) {
+    if (++count < length)
+    {
         document.getElementById("question").innerHTML = "Fråga " + (count + 1) + ": " + questions[count];
-    } else {
+    }
+    else
+    {
         let r1 = document.getElementById("row-1");
         let r2 = document.getElementById("row-2");
         let r3 = document.getElementById("row-3");
