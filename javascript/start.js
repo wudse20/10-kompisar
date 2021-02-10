19beb36f4279adb351fd1fe27cfc157284064998
wudse20 <antonskorup@gmail.com>
2021-02-10 22:43:43 +0100

    Small changes

--- javascript/start.js
+++ javascript/start.js
@@ -9,9 +9,9 @@ function startGame()
     let add = document.getElementById("add");
 
     if (ten.checked)
-        swapPage("..\\html\\10Buddies.html");
+        swapPage(".\\html\\10Buddies.html");
     else if (add.checked)
-        swapPage("..\\html\\add.html");
+        swapPage(".\\html\\add.html");
     else
         console.log("Wierd input form user");
 }
