6f821d2634b0c480a69cfa33e9f2ac3cb8698fce
wudse20 <antonskorup@gmail.com>
2021-02-18 19:43:10 +0100

    Added super type for all questions

--- javascript/question.js
+++ javascript/question.js
@@ -1,80 +1,7 @@
 "use strict";
-class TenBuddiesQuestion {
-    constructor(n1) {
-        this.n1 = n1;
-        this.ans = NaN;
-    }
-    
-    clone() {
-        return new TenBuddiesQuestion(this.n1);
-    }
-
-    checkAnswer(answer) {
-        this.ans = (isNaN(answer)) ? -1 : answer;
-        return !isNaN(+answer) && (10 - +answer === +this.n1);
-    }
-    
-    toString() {
-        let text = isNaN(this.ans) ? "Tiokompis till " + this.n1 + " är _" :
-                                     "Tiokompis till " + this.n1 + " är " + 
-                                     (10 - +this.n1) + (this.checkAnswer(this.ans) ? ": Rätt" : ": Fel");
-        return text;
-    }
-}
-
-class AddQuestion {
-    constructor(i1, ans) {
-        this.i1 = i1;
-        this.ans = ans;
-        this.solved = false;
-        this.correct = false;
-    }
-
-    clone() {
-        return new AddQuestion(this.i1, this.ans);
-    }
-
-    checkAnswer(inp) {
-        this.solved = true;
-        this.correct = !isNaN(parseInt(inp)) && (inp + this.i1 == this.ans);
-        return this.correct;
-    }
-
-    toString() {
-        let text = this.solved ? this.i1 + " + " + (parseInt(this.ans) - parseInt(this.i1)) +
-                                 " = " + this.ans + (this.correct ? ": Rätt" : ": Fel") :
-                                 this.i1 + " + _ = " + this.ans;
-        return text;
-    }
-}
-
-class SubQuestion {
-    constructor(i1, i2) {
-        this.i1 = i1;
-        this.i2 = i2;
-        this.ans = NaN;
-    }
-
-    clone() {
-        return new SubQuestion(this.i1, this.i2);
-    }
-    
-    checkAnswer(answer) {
-        this.ans = (isNaN(answer)) ? -1 : answer;
-        return !isNaN(answer) && (parseInt(this.i1) - parseInt(this.i2) === parseInt(answer));
-    }
-    
-    toString() {
-        let text = isNaN(this.ans) ? this.i1 + " - " + this.i2 + " = _" :
-                                this.i1 + " - " + this.i2 + " = " + (parseInt(this.i1) - parseInt(this.i2)) +
-                                (this.checkAnswer(this.ans) ? ": Rätt" : ": Fel");
-        return text;
-    }
-}
-
-class DoubHalfQuestion {
+class Question {
     constructor(i1) {
-        if (this.constructor == DoubHalfQuestion) {
+        if (this.constructor == Question) {
             throw new Error("Abstract classes can't be instantiated.")
         } else {
             this.i1 = +i1;
@@ -96,7 +23,78 @@ class DoubHalfQuestion {
     }
 }
 
-class DoubleQuestion extends DoubHalfQuestion {
+class TenBuddiesQuestion extends Question {
+    constructor(i1) {
+        super(i1)
+        this.ans = NaN;
+    }
+    
+    clone() {
+        return new TenBuddiesQuestion(this.i1);
+    }
+
+    checkAnswer(answer) {
+        this.ans = (isNaN(answer)) ? -1 : answer;
+        return !isNaN(+answer) && (10 - +answer === +this.i1);
+    }
+    
+    toString() {
+        let text = isNaN(this.ans) ? "Tiokompis till " + this.i1 + " är _" :
+                                     "Tiokompis till " + this.i1 + " är " +
+                                     (10 - +this.i1) + (this.checkAnswer(this.ans) ? ": Rätt" : ": Fel");
+        return text;
+    }
+}
+
+class AddQuestion extends Question {
+    constructor(i1, ans) {
+        super(i1);
+        this.ans = ans;
+    }
+
+    clone() {
+        return new AddQuestion(this.i1, this.ans);
+    }
+
+    checkAnswer(inp) {
+        this.solved = true;
+        this.correct = !isNaN(parseInt(inp)) && (inp + this.i1 == this.ans);
+        return this.correct;
+    }
+
+    toString() {
+        let text = this.solved ? this.i1 + " + " + (parseInt(this.ans) - parseInt(this.i1)) +
+                                 " = " + this.ans + (this.correct ? ": Rätt" : ": Fel") :
+                                 this.i1 + " + _ = " + this.ans;
+        return text;
+    }
+}
+
+class SubQuestion extends Question {
+    constructor(i1, i2) {
+        super(i1);
+        this.i2 = i2;
+        this.ans = NaN;
+    }
+
+    clone() {
+        return new SubQuestion(this.i1, this.i2);
+    }
+    
+    checkAnswer(answer) {
+        this.ans = (isNaN(answer)) ? -1 : answer;
+        return !isNaN(answer) && (parseInt(this.i1) - parseInt(this.i2) === parseInt(answer));
+    }
+    
+    toString() {
+        let text = isNaN(this.ans) ? this.i1 + " - " + this.i2 + " = _" :
+                                this.i1 + " - " + this.i2 + " = " + (parseInt(this.i1) - parseInt(this.i2)) +
+                                (this.checkAnswer(this.ans) ? ": Rätt" : ": Fel");
+        return text;
+    }
+}
+
+class DoubleQuestion extends Question {
     constructor(i1) {
         super(i1);
     }
@@ -118,7 +116,7 @@ class DoubleQuestion extends DoubHalfQuestion {
     }
 }
 
-class HalfQuestion extends DoubHalfQuestion {
+class HalfQuestion extends Question {
     constructor(i1) {
         super(i1);
     }
